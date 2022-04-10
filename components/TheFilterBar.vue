4888892e2abb12703f6dc3c9afbebdab1cade7aa
Stanislav-Kazankov <s.v.kazankov@mail.ru>
2022-04-11 01:06:56 +0900

    Начинает вёрстку панели фильтров

--- components/TheFilterBar.vue
+++ components/TheFilterBar.vue
@@ -1,14 +1,27 @@
 <template lang="pug">
-    .filter-bar
-
+    section.filter-bar
+        h2.visually-hidden
+            | Панель фильтров поиска
+        h3.filter-bar__filter-heading
+            | Количество комнат
+        filter-item-list(:filter-items="roomCountFilterCaptions")
 </template>
 
 <style lang="scss" scoped>
 .filter-bar {
-    padding-top: 41px;
+    padding-top: 42px;
+    padding-right: 33px;
     padding-bottom: 41px;
+    padding-left: 15px;
     border-top-right-radius: 8px;
     border-bottom-right-radius: 8px;
     background-color: $colorWhite;
+
+    &__filter-heading {
+        font-size: 16px;
+        line-height: 20px;
+        font-weight: 600;
+        color: $colorDark;
+    }
 }
 </style>
