0da397b63cff49ce15a35563ce41781bc6c6a8bc
Stanislav-Kazankov <s.v.kazankov@mail.ru>
2022-05-06 22:26:53 +0900

    Выносит панель ползунков слайдера в отдельный компонент

--- components/TheFilterBar/components/BaseDoubleRange/BaseDoubleRange.vue
+++ components/TheFilterBar/components/BaseDoubleRange/BaseDoubleRange.vue
@@ -3,44 +3,47 @@
         lesser-value-field(
             :min-value="minBound"
             :max-value="maxLesserValue"
-            :value="lesserValue"
-            @trigger-value-update="onLesserValueTriggerUpdate"
+            :value="fieldLesserValue"
+            @trigger-value-update="\
+                fieldLesserValue =\
+                    handleBarLesserValue = $event\
+            "
         )
         greater-value-field(
             :min-value="minGreaterValue"
             :max-value="maxBound"
-            :value="greaterValue"
-            @trigger-value-update="onGreaterValueTriggerUpdate"
+            :value="fieldGreaterValue"
+            @trigger-value-update="\
+                fieldGreaterValue =\
+                    handleBarGreaterValue = $event\
+            "
         )
-        .double-range__scale(
-            ref="scale"
-            @click="onScaleClick"
+        handle-bar(
+            ref="handleBar"
+            :min-bound="minBound"
+            :max-bound="maxBound"
+            :lesser-value="handleBarLesserValue"
+            :greater-value="handleBarGreaterValue"
+            @trigger-lesser-value-update="\
+                fieldLesserValue = $event\
+            "
+            @trigger-greater-value-update="\
+                fieldGreaterValue = $event\
+            "
         )
-            .double-range__selection(
-                ref="selection"
-                @click="onSelectionClick"
-            )
-            .double-range__handle.double-range__handle--left(
-                ref="leftHandle"
-                @mousedown.prevent="onHandleMouseDown"
-            )
-            .double-range__handle.double-range__handle--right(
-                ref="rightHandle"
-                @mousedown.prevent="onHandleMouseDown"
-            )
 </template>
 
 <script>
-import _ from 'lodash';
-import $ from 'jquery';
 import LesserValueField from './components/LesserValueField.vue';
 import GreaterValueField from './components/GreaterValueField.vue';
-import { createNumberPropConfig, createFunctionPropConfig } from '@/modules/propConfigs';
+import HandleBar from './components/HandleBar/HandleBar.vue';
+import { createNumberPropConfig } from '@/modules/propConfigs';
 
 export default {
     components: {
         LesserValueField,
         GreaterValueField,
+        HandleBar,
     },
     props: {
         minBound:
@@ -52,189 +55,41 @@ export default {
                 Number.POSITIVE_INFINITY,
             ),
         initialLesserValue:
-            createNumberPropConfig(
-                null,
-            ),
+            createNumberPropConfig(null),
         initialGreaterValue:
-            createNumberPropConfig(
-                null,
-            ),
-        parse:
-            createFunctionPropConfig(
-                calculatedRealFieldValue =>
-                    parseInt(calculatedRealFieldValue, 10),
-            ),
+            createNumberPropConfig(null),
     },
     data() {
         return {
             $document: null,
-            lesserValue: this.minBound,
-            greaterValue: this.maxBound,
+            fieldLesserValue: this.minBound,
+            fieldGreaterValue: this.maxBound,
+            handleBarLesserValue: this.minBound,
+            handleBarGreaterValue: this.maxBound,
             maxLesserValue: this.maxBound,
             minGreaterValue: this.minBound,
-            $leftHandle: null,
-            $rightHandle: null,
-            $scale: null,
-            $selection: null,
-            scaleWidth: null,
-            handleWidth: null,
-            handleHalf: null,
-            leftHandleMinPosition: null,
-            rightHandleMaxPosition: null,
+            handleBar: null,
         };
     },
-    created() {
-        this.lesserValue = this.initialLesserValue || this.minBound;
-        this.greaterValue = this.initialGreaterValue || this.maxBound;
-    },
-    mounted() {
-        this.$document = $(document);
-        this.$scale = $(this.$refs.scale);
-        this.$selection = $(this.$refs.selection);
-        this.scaleWidth = this.$scale.width();
-        this.$leftHandle = $(this.$refs.leftHandle);
-        this.$rightHandle = $(this.$refs.rightHandle);
-        this.handleWidth = this.$leftHandle.width();
-        this.handleHalf = this.handleWidth / 2;
-        this.leftHandleMinPosition = -this.handleHalf;
-        this.rightHandleMaxPosition =
-            this.scaleWidth - this.handleHalf;
-        this.autoSetLeftHandle();
-        this.autoSetRightHandle();
-    },
-    methods: {
-        bindedOnMouseUp: () => {},
-        bindedOnMouseMove: () => {},
-        onLesserValueTriggerUpdate(newValue) {
-            this.lesserValue = newValue;
-            if (this.$leftHandle) {
-                this.autoSetLeftHandle();
-            }
+    watch: {
+        fieldLesserValue(newValue) {
             this.minGreaterValue = newValue;
         },
-        onGreaterValueTriggerUpdate(newValue) {
-            this.greaterValue = newValue;
-            if (this.$rightHandle) {
-                this.autoSetRightHandle();
-            }
+        fieldGreaterValue(newValue) {
             this.maxLesserValue = newValue;
         },
-        onHandleMouseDown($event) {
-            const $handle = $($event.target);
-            $handle.css('z-index', '1');
-            if ($handle[0] === this.$leftHandle[0]) {
-                this.$rightHandle.css('z-index', '0');
-            } else {
-                this.$leftHandle.css('z-index', '0');
-            }
-            const innerOffsetLeft =
-                $event.clientX - $handle.offset().left;
-            this.bindedOnMouseMove = this.onHandleMouseMove
-                .bind(null, $handle, innerOffsetLeft);
-            this.$document.on('mousemove', this.bindedOnMouseMove);
-            this.bindedOnMouseUp = this.onMouseUp.bind(this);
-            this.$document.on('mouseup', this.bindedOnMouseUp);
+        handleBarLesserValue(newValue) {
+            this.minGreaterValue = newValue;
         },
-        onHandleMouseMove($capturedHandle, innerOffsetLeft, $event) {
-            const { $scale, $leftHandle, $rightHandle } = this;
-            $event.preventDefault();
-            const newPosition =
-                $event.clientX - $scale.offset().left - innerOffsetLeft;
-            if ($capturedHandle[0] === $leftHandle[0]) {
-                this.manualSetLeftHandle(newPosition);
-            }
-            if ($capturedHandle[0] === $rightHandle[0]) {
-                this.manualSetRightHandle(newPosition);
-            }
+        handleBarGreaterValue(newValue) {
+            this.maxLesserValue = newValue;
         },
-        onMouseUp($event) {
-            $event.preventDefault();
-            this.$document.off('mousemove', this.bindedOnMouseMove);
-            this.$document.off('mouseup', this.bindedOnMouseUp);
-        },
-        onScaleClick($event) {
-            const { clientX } = $event;
-            const { $leftHandle, $rightHandle, handleWidth } = this;
-            if (clientX < $leftHandle.offset().left) {
-                this.setHandleToClientX('Left', clientX);
-            } else if (clientX > $rightHandle.offset().left + handleWidth) {
-                this.setHandleToClientX('Right', clientX);
-            }
-        },
-        onSelectionClick($event) {
-            const { clientX } = $event;
-            const { $selection } = this;
-            const selectionHalf = $selection.width() / 2;
-            if (clientX <= $selection.offset().left + selectionHalf) {
-                this.setHandleToClientX('Left', clientX);
-            } else {
-                this.setHandleToClientX('Right', clientX);
-            }
-        },
-        setHandleToClientX(handleLocation, clientX) {
-            const { $scale, handleHalf } = this;
-            this[`manualSet${handleLocation}Handle`](
-                clientX - $scale.offset().left - handleHalf,
-            );
-        },
-        autoSetLeftHandle() {
-            const { lesserValue, maxBound, scaleWidth, handleHalf } = this;
-            this.setLeftHandle(
-                lesserValue / maxBound * scaleWidth - handleHalf,
-            );
-        },
-        autoSetRightHandle() {
-            const { greaterValue, maxBound, scaleWidth, handleHalf } = this;
-            this.setRightHandle(
-                greaterValue / maxBound * scaleWidth - handleHalf,
-            );
-        },
-        manualSetLeftHandle(newHandlePosition) {
-            this.setLeftHandle(newHandlePosition);
-            const { $leftHandle, scaleWidth, handleHalf, maxBound } = this;
-            const leftCenterPosition =
-                $leftHandle.position().left + handleHalf + 1;
-            this.lesserValue =
-                this.parse(leftCenterPosition / scaleWidth * maxBound);
-        },
-        manualSetRightHandle(newHandlePosition) {
-            this.setRightHandle(newHandlePosition);
-            const { $rightHandle, scaleWidth, handleHalf, maxBound } = this;
-            const rightCenterPosition =
-                $rightHandle.position().left + handleHalf + 1;
-            this.greaterValue =
-                this.parse(rightCenterPosition / scaleWidth * maxBound);
-        },
-        setLeftHandle(newHandlePosition) {
-            const { handleHalf, leftHandleMinPosition } = this;
-            const rightHandlePosition = this.$rightHandle.position().left;
-            const validHandlePosition = _.clamp(
-                newHandlePosition,
-                leftHandleMinPosition,
-                rightHandlePosition + 1,
-            );
-            this.$leftHandle
-                .css('left', validHandlePosition - 1 + 'px');
-            $(this.$refs.selection)
-                .css('left', validHandlePosition + handleHalf + 'px');
-            const newSelectionWidth = rightHandlePosition - validHandlePosition;
-            $(this.$refs.selection)
-                .css('width', newSelectionWidth + 1 + 'px');
-        },
-        setRightHandle(newHandlePosition) {
-            const { rightHandleMaxPosition } = this;
-            const leftHandlePosition = this.$leftHandle.position().left;
-            const validHandlePosition = _.clamp(
-                newHandlePosition,
-                leftHandlePosition + 1,
-                rightHandleMaxPosition,
-            );
-            this.$rightHandle
-                .css('left', validHandlePosition - 1 + 'px');
-            const newSelectionWidth = validHandlePosition - leftHandlePosition;
-            $(this.$refs.selection)
-                .css('width', newSelectionWidth + 'px');
-        },
+    },
+    created() {
+        this.fieldLesserValue = this.initialLesserValue || this.minBound;
+        this.fieldGreaterValue = this.initialGreaterValue || this.maxBound;
+        this.handleBarLesserValue = this.initialLesserValue || this.minBound;
+        this.handleBarGreaterValue = this.initialGreaterValue || this.maxBound;
     },
 };
 </script>
---
+++ components/TheFilterBar/components/BaseDoubleRange/components/HandleBar/HandleBar.vue
@@ -0,0 +1,221 @@
+<template lang="pug">
+    .handle-bar
+        .handle-bar__scale(
+            ref="scale"
+            @click="onScaleClick"
+        )
+            .handle-bar__selection(
+                ref="selection"
+                @click="onSelectionClick"
+            )
+            .handle-bar__handle.handle-bar__handle--left(
+                ref="leftHandle"
+                @mousedown.prevent="onHandleMouseDown"
+            )
+            .handle-bar__handle.handle-bar__handle--right(
+                ref="rightHandle"
+                @mousedown.prevent="onHandleMouseDown"
+            )
+</template>
+
+<script>
+import _ from 'lodash';
+import $ from 'jquery';
+import { createNumberPropConfig } from '@/modules/propConfigs';
+
+export default {
+    inject: {
+        parse: {
+            default: () =>
+                calculatedRealFieldValue =>
+                    parseInt(calculatedRealFieldValue, 10),
+        },
+    },
+    props: {
+        minBound:
+            createNumberPropConfig(
+                Number.NEGATIVE_INFINITY,
+            ),
+        maxBound:
+            createNumberPropConfig(
+                Number.POSITIVE_INFINITY,
+            ),
+        lesserValue:
+            createNumberPropConfig(
+                Number.NEGATIVE_INFINITY,
+            ),
+        greaterValue:
+            createNumberPropConfig(
+                Number.POSITIVE_INFINITY,
+            ),
+    },
+    data() {
+        return {
+            $leftHandle: null,
+            $rightHandle: null,
+            $scale: null,
+            $selection: null,
+            scaleWidth: null,
+            handleWidth: null,
+            handleHalf: null,
+            leftHandleMinPosition: null,
+            rightHandleMaxPosition: null,
+        };
+    },
+    watch: {
+        lesserValue() {
+            if (this.$leftHandle) {
+                this.autoSetLeftHandle();
+            }
+        },
+        greaterValue() {
+            if (this.$rightHandle) {
+                this.autoSetRightHandle();
+            }
+        },
+    },
+    mounted() {
+        this.$document = $(document);
+        this.$scale = $(this.$refs.scale);
+        this.$selection = $(this.$refs.selection);
+        this.scaleWidth = this.$scale.width();
+        this.$leftHandle = $(this.$refs.leftHandle);
+        this.$rightHandle = $(this.$refs.rightHandle);
+        this.handleWidth = this.$leftHandle.width();
+        this.handleHalf = this.handleWidth / 2;
+        this.leftHandleMinPosition = -this.handleHalf;
+        this.rightHandleMaxPosition =
+            this.scaleWidth - this.handleHalf;
+        this.autoSetLeftHandle();
+        this.autoSetRightHandle();
+    },
+    methods: {
+        onHandleMouseDown($event) {
+            const $handle = $($event.target);
+            $handle.css('z-index', '1');
+            if ($handle[0] === this.$leftHandle[0]) {
+                this.$rightHandle.css('z-index', '0');
+            } else {
+                this.$leftHandle.css('z-index', '0');
+            }
+            const innerOffsetLeft =
+                $event.clientX - $handle.offset().left;
+            this.bindedOnMouseMove = this.onHandleMouseMove
+                .bind(null, $handle, innerOffsetLeft);
+            this.$document.on('mousemove', this.bindedOnMouseMove);
+            this.bindedOnMouseUp = this.onMouseUp.bind(this);
+            this.$document.on('mouseup', this.bindedOnMouseUp);
+        },
+        onHandleMouseMove($capturedHandle, innerOffsetLeft, $event) {
+            const { $scale, $leftHandle, $rightHandle } = this;
+            $event.preventDefault();
+            const newPosition =
+                $event.clientX - $scale.offset().left - innerOffsetLeft;
+            if ($capturedHandle[0] === $leftHandle[0]) {
+                this.manualSetLeftHandle(newPosition);
+            }
+            if ($capturedHandle[0] === $rightHandle[0]) {
+                this.manualSetRightHandle(newPosition);
+            }
+        },
+        onMouseUp($event) {
+            $event.preventDefault();
+            this.$document.off('mousemove', this.bindedOnMouseMove);
+            this.$document.off('mouseup', this.bindedOnMouseUp);
+        },
+        onScaleClick($event) {
+            const { clientX } = $event;
+            const { $leftHandle, $rightHandle, handleWidth } = this;
+            if (clientX < $leftHandle.offset().left) {
+                this.setHandleToClientX('Left', clientX);
+            } else if (clientX > $rightHandle.offset().left + handleWidth) {
+                this.setHandleToClientX('Right', clientX);
+            }
+        },
+        onSelectionClick($event) {
+            const { clientX } = $event;
+            const { $selection } = this;
+            const selectionHalf = $selection.width() / 2;
+            if (clientX <= $selection.offset().left + selectionHalf) {
+                this.setHandleToClientX('Left', clientX);
+            } else {
+                this.setHandleToClientX('Right', clientX);
+            }
+        },
+        setHandleToClientX(handleLocation, clientX) {
+            const { $scale, handleHalf } = this;
+            this[`manualSet${handleLocation}Handle`](
+                clientX - $scale.offset().left - handleHalf,
+            );
+        },
+        autoSetLeftHandle() {
+            const { lesserValue, maxBound, scaleWidth, handleHalf } = this;
+            this.setLeftHandle(
+                lesserValue / maxBound * scaleWidth - handleHalf,
+            );
+        },
+        autoSetRightHandle() {
+            const { greaterValue, maxBound, scaleWidth, handleHalf } = this;
+            this.setRightHandle(
+                greaterValue / maxBound * scaleWidth - handleHalf,
+            );
+        },
+        bindedOnMouseUp: () => {},
+        bindedOnMouseMove: () => {},
+        manualSetLeftHandle(newHandlePosition) {
+            this.setLeftHandle(newHandlePosition);
+            const { $leftHandle, scaleWidth, handleHalf, maxBound } = this;
+            const leftCenterPosition =
+                $leftHandle.position().left + handleHalf + 1;
+            this.$emit(
+                'trigger-lesser-value-update',
+                this.parse(leftCenterPosition / scaleWidth * maxBound),
+            );
+        },
+        manualSetRightHandle(newHandlePosition) {
+            this.setRightHandle(newHandlePosition);
+            const { $rightHandle, scaleWidth, handleHalf, maxBound } = this;
+            const rightCenterPosition =
+                $rightHandle.position().left + handleHalf + 1;
+            this.$emit(
+                'trigger-greater-value-update',
+                this.parse(rightCenterPosition / scaleWidth * maxBound),
+            );
+        },
+        setLeftHandle(newHandlePosition) {
+            const { handleHalf, leftHandleMinPosition } = this;
+            const rightHandlePosition = this.$rightHandle.position().left;
+            const validHandlePosition = _.clamp(
+                newHandlePosition,
+                leftHandleMinPosition,
+                rightHandlePosition + 1,
+            );
+            this.$leftHandle
+                .css('left', validHandlePosition - 1 + 'px');
+            $(this.$refs.selection)
+                .css('left', validHandlePosition + handleHalf + 'px');
+            const newSelectionWidth = rightHandlePosition - validHandlePosition;
+            $(this.$refs.selection)
+                .css('width', newSelectionWidth + 1 + 'px');
+        },
+        setRightHandle(newHandlePosition) {
+            const { rightHandleMaxPosition } = this;
+            const leftHandlePosition = this.$leftHandle.position().left;
+            const validHandlePosition = _.clamp(
+                newHandlePosition,
+                leftHandlePosition + 1,
+                rightHandleMaxPosition,
+            );
+            this.$rightHandle
+                .css('left', validHandlePosition - 1 + 'px');
+            const newSelectionWidth = validHandlePosition - leftHandlePosition;
+            $(this.$refs.selection)
+                .css('width', newSelectionWidth + 'px');
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+@import 'handle-bar.scss';
+</style>
